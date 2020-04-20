68403ac14b4a9208a58e541174cebc3188c47b38
Nadia <nadiaayalanogueira@outlook.com>
2020-04-20 17:23:07 -0300

    .

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,10 @@ from django.conf import settings
 from django.utils import timezone
 
 class Post(models.Model):
+    # All the models to be sent to the database must inherit from .Model
+    # Define the properties our object must have #
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #Charfields are textfields with the number of characters limited in SQL. For unilimited fields use TextField#
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
